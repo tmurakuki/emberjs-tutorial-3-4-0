7deac5e38546f79daef1da17dbf96dd3ddb3090a
Tsuyoshi Murakuki <tmurakuki+github@gmail.com>
2020-05-21 14:03:06 -0400

    tutorial done up to section Building a Complex Component

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,13 @@
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-import { click, currentURL, visit } from '@ember/test-helpers';
+import {
+  click,
+  currentURL,
+  visit,
+  fillIn,
+  triggerKeyEvent,
+} from '@ember/test-helpers';
 
 module('Acceptance | list rentals', function (hooks) {
   setupApplicationTest(hooks);
@@ -33,7 +39,22 @@ module('Acceptance | list rentals', function (hooks) {
     );
   });
 
-  test('should filter the list of rentals by city.', async function (assert) {});
+  test('should filter the list of rentals by city.', async function (assert) {
+    await visit('/');
+    await fillIn('.list-filter input', 'seattle');
+    await triggerKeyEvent('.list-filter input', 'keyup', 69);
+    assert.equal(
+      this.element.querySelectorAll('.results .listing').length,
+      1,
+      'should display 1 listing'
+    );
+    assert.ok(
+      this.element
+        .querySelector('.listing .location')
+        .textContent.includes('Seattle'),
+      'should contain 1 listing with location Seattle'
+    );
+  });
 
   test('should show details for a selected rental', async function (assert) {});
 });
